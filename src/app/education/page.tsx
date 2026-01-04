c0177cee5c84ebb03a0fe2314123fcaaad4e4cd8
Jaxen Dutta <jaxendutta@gmail.com>
2026-01-03 19:42:09 -0500

    feat: add Hamster component with interactive speed control and animation

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,6 +1,7 @@
 import { Section } from '@/components/ui/Section';
 import { EducationCard } from '@/components/sections/EducationCard';
 import { education } from '@/data/education';
+import Hamster from '@/components/pets/Hamster';
 
 export default function EducationPage() {
   return (
@@ -10,13 +11,15 @@ export default function EducationPage() {
         subtitle="Academic journey in plant biology and molecular research"
         centered
       >
-        <div className="max-w-4xl mx-auto flex flex-col gap-4">
+        <div className="max-w-4xl mx-auto flex flex-col">
           {education.map((edu, index) => (
-            <EducationCard
-              key={edu.id}
-              education={edu}
-              isLast={index === education.length - 1}
-            />
+            <div key={edu.id}>
+              <EducationCard
+                education={edu}
+                isLast={index === education.length - 1}
+              />
+              {index < education.length - 1 && <Hamster />}
+            </div>
           ))}
         </div>
       </Section>
